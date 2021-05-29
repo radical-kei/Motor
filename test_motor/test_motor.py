d4eee524518e0f53065b0284ef388fc83b89796e
radical-kei <80798852+radical-kei@users.noreply.github.com>
2021-05-29 18:54:15 +0900

    Fix example for motor control by pigpio.

--- test_motor/test_motor.py
+++ test_motor/test_motor.py
@@ -8,16 +8,16 @@ pi = pigpio.pi()
 
 # Forward 20% 
 # IN2 - HI80% : LO20%
-pi.hardware_PWM(IN2_GPIO, 400000, 800000)
+pi.hardware_PWM(IN2_GPIO, 200000, 750000)
 # IN1 - HI100% : LO0%
-pi.hardware_PWM(IN1_GPIO, 400000, 1000000)
+pi.hardware_PWM(IN1_GPIO, 200000, 1000000)
 time.sleep(5)
 
 # Forward 50%
 # IN2 - HI50% : LO50%
-pi.hardware_PWM(IN2_GPIO, 400000, 500000)
+pi.hardware_PWM(IN2_GPIO, 200000, 500000)
 # IN1 - HI100% : LO0%
-pi.hardware_PWM(IN1_GPIO, 400000, 1000000)
+pi.hardware_PWM(IN1_GPIO, 200000, 1000000)
 time.sleep(5)
 
 # Stop
@@ -27,16 +27,16 @@ time.sleep(2)
 
 # Reverse 20%
 # IN1 - HI80% : LO20%
-pi.hardware_PWM(IN1_GPIO, 400000, 800000)
+pi.hardware_PWM(IN1_GPIO, 200000, 750000)
 # IN2 - HI100% : LO0%
-pi.hardware_PWM(IN2_GPIO, 400000, 1000000)
+pi.hardware_PWM(IN2_GPIO, 200000, 1000000)
 time.sleep(5)
 
 # Reverse 50%
 # IN1 - HI50% : LO50%
-pi.hardware_PWM(IN1_GPIO, 400000, 500000)
+pi.hardware_PWM(IN1_GPIO, 200000, 500000)
 # IN2 - HI100% : LO0%
-pi.hardware_PWM(IN2_GPIO, 400000, 1000000)
+pi.hardware_PWM(IN2_GPIO, 200000, 1000000)
 time.sleep(5)
 
 # Stop
